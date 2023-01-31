940eae82894c7dd9d5615b6cd05bbd6f405666a6
Rohit Verma <84142800+rv1281@users.noreply.github.com>
2023-02-01 03:10:35 +0530

    Delete website directory

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteStudent(studentId) {
-  fetch("/delete-student", {
-    method: "POST",
-    body: JSON.stringify({ studentId: studentId }),
-  }).then((_res) => {
-    window.location.href = "/view_attendance";
-  });
-}
